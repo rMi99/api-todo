57bb5a2bdd9927dd8b1f4423469c359224e155a1
Ramisha Gimhana <67881312+rMi99@users.noreply.github.com>
2023-10-06 11:01:38 +0530

    Update Task.php

--- app/Models/Task.php
+++ app/Models/Task.php
@@ -16,10 +16,10 @@ class Task extends Model
     ];
 
     
-// public function user()
-// {
-//     return $this->belongsTo(User::class);
-// }
+public function user()
+{
+    return $this->belongsTo(User::class);
+}
 
 
 }
